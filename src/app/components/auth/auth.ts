5f898d30570311a0795c1f499ce2ef1893c6d109
el castro <fc099199@gmail.com>
2026-02-13 16:54:13 -0500

    Initial commit

---
+++ src/app/components/auth/auth.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.html',
+  styleUrl: './auth.css',
+})
+export class Auth {
+  titulo="hola"
+  autenticando=true
+  mensajeError=""
+  iniciarsesioncongoogle(){}
+
+}
